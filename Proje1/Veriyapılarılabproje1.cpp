9c75b9ca89535261689bf32c6b8a1791e70d1445
Canberk Beştaş <canberkbestas@gmail.com>
2023-10-10 14:18:18 +0300

    Proje 1B complexity N

---
+++ Proje1/Veriyapılarılabproje1.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m;
+    cout << "Enter the  number of elements.";
+    cin >> m;
+    int arr[20];
+    cout << "Enter the elements.";
+    for (int i = 0; i < m; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cout << arr[i] << "";
+    }
+
+        return 0;
+}
+
+
+
