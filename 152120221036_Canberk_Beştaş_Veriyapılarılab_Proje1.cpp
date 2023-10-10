1c76803e77e8a5d103a12d5e8bd5eead37446b88
Canberk Beştaş <canberkbestas@gmail.com>
2023-10-10 13:18:10 +0300

    Questin 1-A complexity N

---
+++ 152120221036_Canberk_Beştaş_Veriyapılarılab_Proje1.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int arr[10];
+    cout << "Enter 10  numbers:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i] ;
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
